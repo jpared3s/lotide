259b7c3c32486cb6677ada739e4d7ec3ef71cf48
jpared3s <jparedes.wall@gmail.com>
2023-01-09 17:29:48 -0500

    lotide assignment

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+
+
+console.log(assertEqual("Raptors","Raptors"));
+console.log(assertEqual(12, 12));
