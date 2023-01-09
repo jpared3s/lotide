95e9cf5f8343d57f38af3dafa4d5e97b8906b1cd
jpared3s <jparedes.wall@gmail.com>
2023-01-09 18:12:46 -0500

    lotide head assignment

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const head = function (array) {
+  return array[0];
+};
+
+console.log(assertEqual(head([5,6,7]), 6));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Raptors"));
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"))
+console.log(assertEqual(head([]), "Hello"))
+
+
+
