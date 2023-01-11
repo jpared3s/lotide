1f73c9cc3561f06ccf2e27b3156770f4dafaa828
jpared3s <jparedes.wall@gmail.com>
2023-01-11 18:26:04 -0500

    letter positions assignment

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let character = sentence[i];
+    if (results[character]) {
+      results[character].push(i);
+    } else {
+      results[character] = [i];
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
