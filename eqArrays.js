32bf0d6e9cf7df637cc6cf527390b5c0c88adc22
jpared3s <jparedes.wall@gmail.com>
2023-01-10 20:04:43 -0500

    assert arrays equal assignment

--- eqArrays.js
+++ eqArrays.js
@@ -7,14 +7,14 @@ const assertEqual = function(actual, expected) {
 };
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return false;  
+    return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i])
-    return false;
+      return false;
   }
   return true;
-}; 
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
