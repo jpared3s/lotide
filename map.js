d56d97e29630e7b12526ea91990d27324c645d22
jpared3s <jparedes.wall@gmail.com>
2023-01-12 18:13:23 -0500

    map lotide assignment

---
+++ map.js
@@ -0,0 +1,33 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const players = ["barnes", "siakam", "fred", "og"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(players, word => word[0]);
+assertArraysEqual(results1, ['b', 's', 'f', 'o']);
+
