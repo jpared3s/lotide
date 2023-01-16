4200413f12f455d6b0b5d888754c69dece89dd54
jpared3s <jparedes.wall@gmail.com>
2023-01-16 15:34:16 -0500

    tailTest and middleTest mocha/chai assignment

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,28 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+
+describe('#middle', () => {
+  it('return an empty array if less than three elements in the array', () => {
+    assert.deepEqual(middle([1], [2]), []);
+  });
+  it('returns middle element if array contains odd number of elements', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it('returns two middle elements if more than 3 elements and has even number of elements', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  })
+})
+
 
 //For arrays with one or two elements, there is no middle. Return an empty array.
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
 
-//For arrays with odd number of elements, an array containing a single middle element should be returned
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
+// //For arrays with odd number of elements, an array containing a single middle element should be returned
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
 
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+// //For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+// assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,12 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail([5,6,7]);
-assertEqual(result.length, 2);
+describe('#tail', () => {
+  it('returns that length of the array tail minus the head', () => {
+    assert.strictEqual(tail([5,6,7]).length, 2);
+  });
+});
+
+// const result = tail([5,6,7]);
+// assertEqual(result.length, 2);
