5fa0ce62448627dd3082359a428a7fb93419ad92
jpared3s <jparedes.wall@gmail.com>
2023-01-16 13:48:32 -0500

    assert Array Equal Test lotide assignment

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i])
-      return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -17,5 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [11, 22, 3]);
+module.exports = assertArraysEqual;
